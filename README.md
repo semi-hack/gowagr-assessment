0e2d22c32223decb9da42822be7f45f0cd45de4e
Akinlo Semiloore <akinlo@onepipe.io>
2024-10-18 21:58:21 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Description
+
+A Simple Bank API 
+
+## Requirements
+
+- Node JS v18+
+- Docker
+
+## Environment variables
+
+Create your personal `.env` file using the `.env.example` file as a guide.
+
+## Dependencies
+
+The program currently relies on the following dependencies
+
+- Postgres
+
+## Installation
+
+```bash
+$ npm install
+```
---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
