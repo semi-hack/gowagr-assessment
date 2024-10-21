d9dc1ee7e1fd4c39198ecbfc1fd44933c440e616
Akinlo Semiloore <akinlo@onepipe.io>
2024-10-21 23:05:09 +0100

    update column transformer comment

--- src/shared/utils/column-transformer.ts
+++ src/shared/utils/column-transformer.ts
@@ -1,10 +1,20 @@
 import { ValueTransformer } from 'typeorm';
 
 export class DecimalColumnToNumberTransformer implements ValueTransformer {
+  /**
+   * Converts a string to a string
+   * @param {string} value - The string value
+   * @returns {string} The string value
+   */
   to(value: string): string {
     return value;
   }
 
+  /**
+   * Converts a string to a number
+   * @param {string} value - The string value
+   * @returns {number} The number value
+   */
   from(value: string): number {
     return value ? parseFloat(value) : null;
   }
